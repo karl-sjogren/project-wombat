3eca066a9745bff635d3f0216562c49ea3ab7417
Karl-Johan Sjogren <karl-johan.sjogren@openlibrarysolutions.com>
2019-04-11 15:28:51 +0200

    Now disabling buttons when not preparing

--- src/ProjectWombat/ClientApp/app/controllers/cashier.js
+++ src/ProjectWombat/ClientApp/app/controllers/cashier.js
@@ -1,9 +1,22 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 import { inject } from '@ember/service';
 
 export default Controller.extend({
   orderService: inject('cashierOrderService'),
   cashierService: inject(),
+
+  disableEditing: computed('orderService.order.status', function() {
+    if(!this.orderService.order) {
+      return true;
+    }
+
+    if(this.orderService.order.status !== 'preparing') {
+      return true;
+    }
+
+    return false;
+  }),
 
   actions: {
     newOrder() {
